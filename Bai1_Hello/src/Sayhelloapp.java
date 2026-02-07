073a8c1fb7d87ce221f4c9aca918ea8fdf32a284
Nguyễn Ngọc Xuân Ánh <xuananhc92324@gmail.com>
2026-02-07 13:18:42 +0700

    Tai voi chay duoc hello roai ne thay

---
+++ Bai1_Hello/src/Sayhelloapp.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+/**
+ * 
+ */
+public class Sayhelloapp {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Helloooooo tai duoc eclipse roi ne thay uiii");
+
+	}
+
+}
