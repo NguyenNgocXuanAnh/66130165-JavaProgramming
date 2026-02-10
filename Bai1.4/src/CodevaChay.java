db4f67a46a49548c579caf2c912f83dad1327143
Nguyễn Ngọc Xuân Ánh <xuananhc92324@gmail.com>
2026-02-10 15:33:52 +0700

    bai 1.4

---
+++ Bai1.4/src/CodevaChay.java
@@ -0,0 +1,17 @@
+
+public class CodevaChay {
+
+	public static void main(String[] args) {
+
+		        // Text blocks với """
+		        String greeting = """
+		            ======================
+		            HELLO, MODERN JAVA!
+		            ======================
+		            by Mai Cường Thọ NTU
+		            ======================
+		            """;
+		        System.out.println(greeting);
+	}
+
+}
