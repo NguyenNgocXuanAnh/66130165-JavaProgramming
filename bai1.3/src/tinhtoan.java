a55f3743d9896d8274f9eaee19cd4b3b350f0d5d
Nguyễn Ngọc Xuân Ánh <xuananhc92324@gmail.com>
2026-02-10 15:27:19 +0700

    bài 1.3

--- bai1.3/src/tinhtoan.java
+++ bai1.3/src/tinhtoan.java
@@ -1,4 +1,22 @@
 
 public class tinhtoan {
-
+	public static void main(String[] args) {
+		
+		// PHÉP CỘNG
+		System.out.println ("- CỘNG:");
+        System.out.println ("5 + 5 = " + (5 + 5));
+        
+        // PHÉP TRỪ
+        System.out.println ("- TRỪ");
+        System.out.println ("5 - 5 = " + (5 - 5));
+        
+        // PHÉP NHÂN
+        System.out.println ("- NHÂN:");
+        System.out.println ("5 * 5 = " + (5 * 5));
+        
+        //PHÉP CHIA
+        System.out.println ("- CHIA:");
+        System.out.println ("5 / 5 = " + (5 / 5));
+    }
 }
+
