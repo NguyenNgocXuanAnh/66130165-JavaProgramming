58424e6d513ed8beb4c6f189985137f8a1c77d7a
Nguyễn Ngọc Xuân Ánh <xuananhc92324@gmail.com>
2026-02-10 15:14:25 +0700

    Create thongtincanhan.java

---
+++ Bai1.1/src/thongtincanhan.java
@@ -0,0 +1,12 @@
+
+public class thongtincanhan {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hi! My full name is Nguyễn Ngọc Xuân Ánh.");
+		System.out.println("My birthday is 02/09/2006");
+		System.out.println("I study at Nha Trang university");
+		System.out.println("hé hé bái bai");
+	}
+
+}
