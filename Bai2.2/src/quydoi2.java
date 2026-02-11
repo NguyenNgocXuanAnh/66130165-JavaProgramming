96491b734c90f8fde169ad0d114734d735e57929
Nguyễn Ngọc Xuân Ánh <xuananhc92324@gmail.com>
2026-02-11 15:30:59 +0700

    hoàn thành EUR -> VND

---
+++ Bai2.2/src/quydoi2.java
@@ -0,0 +1,16 @@
+
+public class quydoi2 {
+
+	public static void main(String[] args) {
+		
+		//EUR sang VND (1 EUR = 27,000 VND)
+		
+		int EUR = 5000;
+		double VND = 27.000;
+		double change = EUR * VND;
+		
+		//hiển thị quy đổi
+		
+		System.out.println (" EUR: " + EUR + " -> VND: " + change);
+	}
+}
