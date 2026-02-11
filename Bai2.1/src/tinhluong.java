8a1a086d7e152ececf964e78d0de073fd5bffeba
Nguyễn Ngọc Xuân Ánh <xuananhc92324@gmail.com>
2026-02-11 15:19:23 +0700

    hoàn thành bài 2.1 tính lương

--- Bai2.1/src/tinhluong.java
+++ Bai2.1/src/tinhluong.java
@@ -2,7 +2,23 @@
 public class tinhluong {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		//khai báo số giờ làm 150h, lương 17 000/h
+		
+		int time = 150;
+		int money = 17000;
+		double total = 0;
+		
+		//tính lương
+		
+		if (time<40)
+			total = (double) time * money;
+		else 
+			total = (double) money * 40 + money * (time - 40) * 1.5;
+		
+		//in lương
+		
+		System.out.println ("- Tổng lương của ban la: " + total);
 
 	}
 
