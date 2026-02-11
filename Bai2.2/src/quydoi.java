ba723dd5e94e5c20af19d601db0f171691af74ef
Nguyễn Ngọc Xuân Ánh <xuananhc92324@gmail.com>
2026-02-11 15:27:38 +0700

    hoàn thành USA -> VND

---
+++ Bai2.2/src/quydoi.java
@@ -0,0 +1,17 @@
+
+public class quydoi {
+
+	public static void main(String[] args) {
+		
+		// USD sang VND (1 USD = 23,500 VND)
+		
+		int USA = 5000;
+		double VND = 23.500;
+		double change = USA * VND;
+		
+		//hiển thị quy đổi
+		
+		System.out.println (" USA: " + USA + " -> VND: " + change);
+	}
+
+}
