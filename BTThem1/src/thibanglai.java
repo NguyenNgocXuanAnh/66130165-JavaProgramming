71d273f191d233d4b13b3aa94fa98b7843c5326d
Nguyễn Ngọc Xuân Ánh <xuananhc92324@gmail.com>
2026-02-12 20:38:52 +0700

    done

--- BTThem1/src/thibanglai.java
+++ BTThem1/src/thibanglai.java
@@ -2,7 +2,12 @@
 public class thibanglai {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		//khai báo tuổi
+		int age = 15;
+		if (age >= 18)
+			System.out.println ("-> Đủ tuổi thi bằng lái xe");
+		else 
+			System.out.println ("-> Chưa đủ tuổi thi bằng lái xe");
 
 	}
 
