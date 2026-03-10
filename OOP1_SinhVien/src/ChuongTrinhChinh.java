b4cb09b3c18e11a327a465f0c9b5267e2c039f5f
Nguyễn Ngọc Xuân Ánh <xuananhc92324@gmail.com>
2026-03-10 22:09:08 +0700

    done

---
+++ OOP1_SinhVien/src/ChuongTrinhChinh.java
@@ -0,0 +1,15 @@
+
+public class ChuongTrinhChinh {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//tạo mới 1 sinh viên
+		sinhvien sv1 = new sinhvien ();
+		//cấp/gán dữ liệu cụ thể 
+		sv1.ten = "Phạm Thị Tươi Tắn Lắm";
+		sv1.tuoi = 18;
+		sv1.mssv = "66112233";
+		sv1.hocBai("Lập trình Java");
+	}
+
+}
---
+++ OOP1_SinhVien/src/sinhvien.java
@@ -0,0 +1,12 @@
+
+public class sinhvien {
+	 // Thuộc tính
+    String ten;
+    int tuoi;
+    String mssv;
+    
+    // Phương thức
+    void hocBai(String monHoc) {
+        System.out.println(ten + " đang học môn " + monHoc);
+    }
+}
