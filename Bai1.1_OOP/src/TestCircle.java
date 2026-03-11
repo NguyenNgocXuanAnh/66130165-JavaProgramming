48125efdb77ba67ac2b073ded2ff5be8f5256b7f
Nguyễn Ngọc Xuân Ánh <xuananhc92324@gmail.com>
2026-03-12 02:17:09 +0700

    code chạy giá trị mặt định

--- Bai1.1_OOP/src/TestCircle.java
+++ Bai1.1_OOP/src/TestCircle.java
@@ -3,7 +3,15 @@ public class TestCircle {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		
+		//tạo 1 đối tượng circle tên c1
+		
+		Circle c1 = new Circle ();
+		
+		//gọi các phương thức công khai trên thể hiện c1
+		
+		System.out.print("Hình tròn có bán kính là: "
+		+ c1.getRadius() + " và diện tích của " + c1.getArea());
 	}
 
 }
