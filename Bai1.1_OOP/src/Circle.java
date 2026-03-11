015273b666dc416ae5cee4ab2bb813cbb18f359d
Nguyễn Ngọc Xuân Ánh <xuananhc92324@gmail.com>
2026-03-12 02:05:59 +0700

    code xong circle

--- Bai1.1_OOP/src/Circle.java
+++ Bai1.1_OOP/src/Circle.java
@@ -6,6 +6,29 @@ public class Circle {
 	private double bk;
 	private String color;
 	
-	//hàm tạo
+	//hàm tạo 1 (mặc định)
 	
+	public Circle() {
+		bk = 1.0;
+		color = "red";
+	}
+	
+	//hàm tạo 2
+	
+	public Circle(double r) {
+		bk = r;
+		color = "red";
+	}
+	
+	//trả về bán kính
+	
+	public double getRadius() {
+		return bk;
+	}
+	
+	//trả về diện tích của đtuong Circle 
+	
+	public double getArea() {
+		return bk * bk * Math.PI;
+	}
 }
