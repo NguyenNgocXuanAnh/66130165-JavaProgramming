307434a83957037adccf2b23c3eb690f10e57a4a
Nguyễn Ngọc Xuân Ánh <xuananhc92324@gmail.com>
2026-03-12 02:58:18 +0700

    code xong lớp hình tròn

--- Bai1.2_OOP/src/Circledifferent.java
+++ Bai1.2_OOP/src/Circledifferent.java
@@ -1,4 +1,53 @@
 
 public class Circledifferent {
+	//khai báo
+	
+		private double bk;
+		private String color;
+		
+		//hàm tạo 1 (mặc định)
+		
+		public Circledifferent() {
+			bk = 1.0;
+			color = "red";
+		}
+		
+		//hàm tạo 2
+		
+		public Circledifferent(double r) {
+			bk = r;
+			color = "red";
+		}
+		
+		//trả về bán kính
+		
+		public double getRadius() {
+			return bk;
+		}
+		
+		//thiết lập bán kính new
+		
+		public void setRadius (double newRadius) {
+			bk = newRadius;
+		}
 
+		//trả về diện tích của đtuong Circle 
+		
+		public double getArea() {
+			return bk * bk * Math.PI;
+		}
+		
+		//chu vi
+		
+		public double getCircumference() {
+			return bk * 2 * Math.PI;
+		}
+		
+		//toString
+		
+		public String toString() {
+		String s =  "Circle [bán kính = " + bk + ", màu = " + color + "]";
+		return s;
+		}
+		
 }
