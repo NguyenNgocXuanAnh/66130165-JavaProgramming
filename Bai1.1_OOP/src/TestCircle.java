0a253793c51d1333d900ed24db44ab1ea9c0cbb3
Nguyễn Ngọc Xuân Ánh <xuananhc92324@gmail.com>
2026-03-12 02:22:03 +0700

    done

--- Bai1.1_OOP/src/TestCircle.java
+++ Bai1.1_OOP/src/TestCircle.java
@@ -10,8 +10,19 @@ public class TestCircle {
 		
 		//gọi các phương thức công khai trên thể hiện c1
 		
-		System.out.print("Hình tròn có bán kính là: "
+		System.out.println("Hình tròn có bán kính là: "
 		+ c1.getRadius() + " và diện tích của " + c1.getArea());
+	
+	    // Khai báo một thể hiện của lớp circle có tên là c2.
+	    // Khởi tạo thể hiện c2 bằng cách gọi hàm tạo thứ hai
+	    // với bán kính và màu sắc mặc định đã cho.
+	
+		Circle c2 = new Circle(2.0);
+		
+		// Gọi các phương thức công khai trên thể hiện c2, thông qua toán tử dấu chấm.
+	   
+		System.out.println("Hình tròn có bán kính là: "
+	    + c2.getRadius() + " và diện tích của " + c2.getArea());
 	}
 
 }
