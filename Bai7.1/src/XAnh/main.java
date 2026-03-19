40228993d212ca1561efb018660f1147dd2a568e
Nguyễn Ngọc Xuân Ánh <xuananhc92324@gmail.com>
2026-03-19 07:34:12 +0700

    Update main.java

--- Bai7.1/src/XAnh/main.java
+++ Bai7.1/src/XAnh/main.java
@@ -5,6 +5,16 @@ public class main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+	SanPham sp1 = new SanPham();
+	System.out.println(sp1);
+				
+	SanPham sp2 = new SanPham("A010", "pencil", 3000, 15);
+	System.out.println(sp2);
+	sp2.setGia(4000);
+	System.out.println("-> Sau khi thay doi gia: ");
+	System.out.println(sp2);
+	System.out.println("Tong tien: " + sp2.tinhThanhTien());
+
 	}
 
 }
