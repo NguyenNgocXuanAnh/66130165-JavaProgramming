d1364b2f9b3b3c039dbaabb3762bc52d28881962
Nguyễn Ngọc Xuân Ánh <xuananhc92324@gmail.com>
2026-03-19 09:30:41 +0700

    Update NhanVien.java

--- OOP3_KeThua_QLNV/src/anhnnx/quanlynhanvien/NhanVien.java
+++ OOP3_KeThua_QLNV/src/anhnnx/quanlynhanvien/NhanVien.java
@@ -15,5 +15,17 @@ public class NhanVien {
 		hoTen = tenNhanVien;
 		this.luongCoBan = luongCoBan;
 	}
-	
+	public double tinhLuong () {
+		return luongCoBan;
+	}
+
+	@Override
+	public String toString() {
+		return "NhanVien [maNV=" + maNV + ", hoTen=" + hoTen + ", luongCoBan=" + luongCoBan + "]";
+	}
+	public void hienThiThongTin () {
+		System.out.println ("Mã NV: " + maNV);
+		System.out.println ("Họ Tên: " + hoTen);
+		System.out.println ("Lương cơ bản: " + luongCoBan);
+	}
 }
