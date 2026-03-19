6c19a41908591133a8833a99d6c7a1dd38c828db
Nguyễn Ngọc Xuân Ánh <xuananhc92324@gmail.com>
2026-03-19 09:19:36 +0700

    Update NhanVien.java

--- OOP3_KeThua_QLNV/src/anhnnx/quanlynhanvien/NhanVien.java
+++ OOP3_KeThua_QLNV/src/anhnnx/quanlynhanvien/NhanVien.java
@@ -1,5 +1,19 @@
 package anhnnx.quanlynhanvien;
 
 public class NhanVien {
-
+	//thuộc tính
+	private String maNV;
+	private String hoTen;
+	private double luongCoBan;
+	//hàm tạo
+	public NhanVien () {
+		
+	}
+	
+	public NhanVien (String maNhanVien, String tenNhanVien, double luongCoban) {
+		maNV = maNhanVien;
+		hoTen = tenNhanVien;
+		this.luongCoBan = luongCoBan;
+	}
+	
 }
