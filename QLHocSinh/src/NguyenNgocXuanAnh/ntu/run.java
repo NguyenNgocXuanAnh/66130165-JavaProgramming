201531f8b754b8ed4339991953eba8689ac6356d
Nguyễn Ngọc Xuân Ánh <xuananhc92324@gmail.com>
2026-04-02 00:14:05 +0700

    xong câu 2b

--- QLHocSinh/src/NguyenNgocXuanAnh/ntu/run.java
+++ QLHocSinh/src/NguyenNgocXuanAnh/ntu/run.java
@@ -1,5 +1,5 @@
 package NguyenNgocXuanAnh.ntu;
-
+import java.util.Scanner;
 public class run {
 
 	public static void main(String[] args) {
@@ -23,6 +23,29 @@ public class run {
 		System.out.print("\nTên của HS 2 là: " + hs2.getTenHS() );
 		System.out.print("\nTuổi của HS 2 là: " + hs2.getTuoiHS() );
 		System.out.print("\nLớp của HS 2 là: " + hs2.getLopHS() );
+		
+		// NHẬP DỮ LIỆU TỪ BÀN PHÍM
+		Scanner banPhim = new Scanner (System.in);
+		// Nhập dữ liệu học sinh thứ 3
+		HocSinh hs3 = new HocSinh ();
+		 // hỏi user nhập vào 
+		System.out.print("\nMời nhập tên của của học sinh thứ 3: "); //in ra hd và đợi
+		String tenHS3 = banPhim.nextLine();
+		//gói vào biến hs3
+		hs3.setTenHS(tenHS3);
+		
+		System.out.print("Mời nhập tuổi của học sinh thứ 3: ");
+		short tuoiHS3 = banPhim.nextShort();
+		hs3.setTuoiHS(tuoiHS3);
+		banPhim.nextLine();
+		
+		System.out.print("Mời nhập lớp của học sinh thứ 3: ");
+		String lopHS3 = banPhim.nextLine();
+		hs3.setLopHS(lopHS3);
+		 
+		//In hs 3 ra màn hình 
+		
+		System.out.print(hs3.toString() );
 		
 	}
 
