5ccb2f9627f67e8b6216be18b3d07c95373f2e12
Nguyễn Ngọc Xuân Ánh <xuananhc92324@gmail.com>
2026-04-13 10:37:21 +0700

    code TG

--- BTThem2/src/NguyeNgocXuanAnh/TamGiac.java
+++ BTThem2/src/NguyeNgocXuanAnh/TamGiac.java
@@ -48,19 +48,27 @@ public class TamGiac {
 			return 0;
 		}
 	}
-	public void loạiTG () {
-		if (kiemtraTG (a,b,c) == 1) {
-			if ((a == b) && (b == c))
-				System.out.println ("Đây là tam giac đều");
-			else if ((a *a + b *b == c *c) || (a *a + c *c == b *b) || (b *b + c *c == a *a))
-				System.out.println ("Đây là tam giác vuông");
-			else if ((a == b) || (a == c) || (b == c));
-				System.out.println ("Đây là tam giác cân");
-			else if (((a *a + b *b == c *c) || (a *a + c *c == b *b) || (b *b + c *c == a *a))
-						&& ((a == b) || (a == c) || (b == c)))
-				System.out.println("Đây là tam giac vuông cân");
-		}
+	//nếu là tam giac thì a, b, c là tam giác gì
+	public void loạiTG (float a, float b, float c) {
+		if ((a == b) && (b == c))
+			System.out.println ("Đây là tam giac đều");
+		else if ((a *a + b *b == c *c) || (a *a + c *c == b *b) || (b *b + c *c == a *a))
+			System.out.println ("Đây là tam giác vuông");
+		else if ((a == b) || (a == c) || (b == c))
+			System.out.println ("Đây là tam giác cân");
+		else if (((a *a + b *b == c *c) || (a *a + c *c == b *b) || (b *b + c *c == a *a))
+					&& ((a == b) || (a == c) || (b == c)))
+			System.out.println ("Đây là tam giac vuông cân");
+		else 
+			System.out.println ("Đây là tam giac thường");
 		
+	}
+	public float ChuVi (float a, float b, float c) {
+		return (a + b + c)/2;
+	}
+	public double DienTich (float a, float b, float c) {
+		float p = (a + b + c)/2;
+		return Math.sqrt(p*(p-a)*(p-b)*(p-c));
 	}
 	
 }
