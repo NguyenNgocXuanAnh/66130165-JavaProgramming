9615df5abe93dc5db273194958cdf42f282d4924
Nguyễn Ngọc Xuân Ánh <xuananhc92324@gmail.com>
2026-04-15 23:10:22 +0700

    .

--- ViDu_StreamDocChuoi2/src/NguyenNgocXuanAnh/ViDuDocStream.java
+++ ViDu_StreamDocChuoi2/src/NguyenNgocXuanAnh/ViDuDocStream.java
@@ -1,10 +1,34 @@
 package NguyenNgocXuanAnh;
 
+import java.io.InputStreamReader;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.FileInputStream;
+import java.io.FileReader;
+
 public class ViDuDocStream {
+	public static void main(String[] args) throws IOException {
+		
+		FileInputStream fInput = new FileInputStream ("BaiTho.txt");
+		
+		// Khai báo biến đối tượng InputStreamReader
+		InputStreamReader ipReader = new InputStreamReader(fInput);
+		// Khai báo biến đối tượng Buffer
+		BufferedReader bufReader = new BufferedReader(ipReader);
+	
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		// Đọc 1 dòng dữ liệu từ file, cất vào biến chuỗi
+		String line1 = bufReader.readLine();
+		// In ra dòng đọc được
+		System.out.println(line1);
+		
+		// Đọc 1 dòng dữ liệu từ file, cất vào biến chuỗi
+		String line2 = bufReader.readLine();
+		// In ra dòng đọc được
+		System.out.println(line2);
+	
 	}
+	
 
 }
+
