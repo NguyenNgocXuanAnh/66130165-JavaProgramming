288c3e1db9eac2edfbdc0ba293a88e99af71a332
Nguyễn Ngọc Xuân Ánh <xuananhc92324@gmail.com>
2026-04-15 22:48:35 +0700

    .

--- ViDu_StreamDocChuoi/src/NguyenNgocXuanAnh/ViDuDocStream.java
+++ ViDu_StreamDocChuoi/src/NguyenNgocXuanAnh/ViDuDocStream.java
@@ -1,5 +1,24 @@
 package NguyenNgocXuanAnh;
 
+import java.io.InputStreamReader;
+import java.io.BufferedReader;
+import java.io.IOException;
+
 public class ViDuDocStream {
+	public static void main(String[] args) throws IOException {
+		// Khai báo biến đối tượng InputStreamReader
+		InputStreamReader ipReader = new InputStreamReader(System.in);
+		// Khai báo biến đối tượng Buffer
+		BufferedReader bufReader = new BufferedReader(ipReader);
+	
+		// In một câu hướng dẫn để User biết cần làm gì
+		System.out.print("Nhập một chuỗi bất kỳ: ");
+		// Đọc dữ liệu từ bàn phím, cất vào biến chuỗi
+			String lineDocDuoc = bufReader.readLine();
+		// In ra dòng đọc được
+			System.out.print(lineDocDuoc);
+	
+	}
+	
 
 }
