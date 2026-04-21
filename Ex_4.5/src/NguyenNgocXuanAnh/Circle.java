86fa5a85d56df32ea820c32b3d27f4c20cd56a8b
Nguyễn Ngọc Xuân Ánh <xuananhc92324@gmail.com>
2026-04-21 19:37:20 +0700

    xong Circle

--- Ex_4.5/src/NguyenNgocXuanAnh/Circle.java
+++ Ex_4.5/src/NguyenNgocXuanAnh/Circle.java
@@ -1,5 +1,46 @@
 package NguyenNgocXuanAnh;
 
-public class Circle {
+public class Circle extends Shape {
+	private double radious;
 
+	
+	public Circle() {
+		super();
+
+	}
+
+
+	public Circle(String color, boolean filled, double radious) {
+		super(color, filled);
+		this.radious = radious;
+	}
+
+
+	public double getRadious() {
+		return radious;
+	}
+
+
+	public void setRadious(double radious) {
+		this.radious = radious;
+	}
+	
+	public double getArea (double radius) {
+		return radius*radius*Math.PI;
+	}
+	
+	 public double getPerimeter (double radius) {
+		 return radius*2*Math.PI;
+	}
+
+
+	 @Override
+	 public String toString() {
+		return "Circle [radious=" + radious + ", color=" + color + ", filled=" + filled + ", getRadious()="
+				+ getRadious() + ", getColor()=" + getColor() + ", isFilled()=" + isFilled() + ", toString()="
+				+ super.toString() + ", getClass()=" + getClass() + ", hashCode()=" + hashCode() + "]";
+	 }
+	 
+	
+	
 }
