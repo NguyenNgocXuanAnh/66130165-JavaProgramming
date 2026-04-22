005741b1b896281522e489efba2ac2ff536a3130
Nguyễn Ngọc Xuân Ánh <xuananhc92324@gmail.com>
2026-04-22 19:12:46 +0700

    coding -> Square

--- Ex_4.5/src/NguyenNgocXuanAnh/Square.java
+++ Ex_4.5/src/NguyenNgocXuanAnh/Square.java
@@ -1,8 +1,31 @@
 package NguyenNgocXuanAnh;
 
-public class Square extends Ractangle {
+public class Square extends Rectangle {
 	public Square(double side) { 
 		   super(side, side);   // Gọi lớp cha Rectangle(double, double) 
 		}
+
+	public Square() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public Square(double side, double side1) {
+		super(side1, side1);
+		// TODO Auto-generated constructor stub
+	}
+
+	public Square(String color, boolean filled, double side) {
+		super(color, filled, side, side);
+		// TODO Auto-generated constructor stub
+	}
+	
+	public double getSide () {
+		return this.getLength();
+	}
+	 public void setSide (double side) {
+		 this.setLength(side);
+	 }
+	 
 
 }
