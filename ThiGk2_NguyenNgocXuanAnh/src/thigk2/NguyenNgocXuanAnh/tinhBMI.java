d280373be02def8d474b385a2c551b1fef28199b
Nguyễn Ngọc Xuân Ánh <xuananhc92324@gmail.com>
2026-04-23 07:46:06 +0700

    lần thứ 2

--- ThiGk2_NguyenNgocXuanAnh/src/thigk2/NguyenNgocXuanAnh/tinhBMI.java
+++ ThiGk2_NguyenNgocXuanAnh/src/thigk2/NguyenNgocXuanAnh/tinhBMI.java
@@ -2,16 +2,25 @@ package thigk2.NguyenNgocXuanAnh;
 import java.util.Scanner; 
 public class tinhBMI {
 		public static void main(String[] args) {
-	        // BMI = cân nặng (kg) / (chiều cao (m) * chiều cao (m))
+			//hỏi user nhập vào
 	        Scanner bp = new Scanner (System.in);
-	        double height;    // m
 	        System.out.println("Nhập chiều cao, cân nặng: ");
-	        //nhâp cân nặng
-			System.out.println("Nhập cân nặng: ");
-			double weight = bp.nextDouble(); //nhâp cân nặng
-	        double bmi = weight1 / (height * height);
 	        
-	        System.out.println("Cân nặng: " + weight1 + " kg");
+	        //nhâp cân nặng
+	        System.out.println("Nhập cân nặng: ");
+			double weight = bp.nextDouble(); //nhâp cân nặng
+			
+			//nhập chiều cao
+			System.out.println("Nhập chiều cao: ");
+			double height = bp.nextDouble(); //nhâp chiều cao
+			if (height <= 0 || weight <= 0) {
+			    System.out.println("Dữ liệu không hợp lệ!");
+			    return;
+			}
+	        // BMI = cân nặng (kg) / (chiều cao (m) * chiều cao (m))
+	        double bmi = weight / (height * height);
+	        
+	        System.out.println("Cân nặng: " + weight + " kg");
 	        System.out.println("Chiều cao: " + height + " m");
 	        System.out.println("Chỉ số BMI: " + bmi);
 	        
