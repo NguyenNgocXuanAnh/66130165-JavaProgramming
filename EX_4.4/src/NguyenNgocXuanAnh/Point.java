7dc13814107f0b65ce2562e567bfa78dd266f1fd
Nguyễn Ngọc Xuân Ánh <xuananhc92324@gmail.com>
2026-05-11 05:24:28 +0700

    .

--- EX_4.4/src/NguyenNgocXuanAnh/Point.java
+++ EX_4.4/src/NguyenNgocXuanAnh/Point.java
@@ -1,5 +1,50 @@
 package NguyenNgocXuanAnh;
 
 public class Point {
+	private float x;
+	private float y;
+	
+	public Point() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
 
+	public Point(float x, float y) {
+		super();
+		this.x = x;
+		this.y = y;
+	}
+
+	public float getX() {
+		return x;
+	}
+
+	public void setX(float x) {
+		this.x = x;
+	}
+
+	public float getY() {
+		return y;
+	}
+
+	public void setY(float y) {
+		this.y = y;
+	}
+	
+	public float[] getXY() { 
+	       float[] result = new float[2];   // tạo một mảng gồm 2 phần tử 
+	       result[0] = x; 
+	       result[1] =  y;
+	       return result;   // trả về mảng 
+	}
+	
+	public void setXY (float x, float y) {
+		this.x = x;
+		this.y = y;
+	}
+
+	@Override
+	public String toString() {
+		return "(" + x + ", " + y + ")";
+	}
 }
