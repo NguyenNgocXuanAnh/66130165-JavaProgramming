3acf025e996615c27e1c0a586715f7fcf35b1f58
Nguyễn Ngọc Xuân Ánh <xuananhc92324@gmail.com>
2026-05-11 04:03:58 +0700

    tạo lớp Circle

---
+++ EX_4.1/src/NguyenNgocXuanAnh/Circle.java
@@ -0,0 +1,57 @@
+package NguyenNgocXuanAnh;
+
+public class Circle {
+	private double radius;
+	private String color;
+	
+	
+	public Circle() {
+		super();
+		radius = 1.0;
+		color = "red";
+	}
+
+
+	public Circle(double radius) {
+		super();
+		this.radius = radius;
+	}
+
+
+	public Circle(double radius, String color) {
+		super();
+		this.radius = radius;
+		this.color = color;
+	}
+
+
+	public double getRadius() {
+		return radius;
+	}
+
+
+	public void setRadius(double radius) {
+		this.radius = radius;
+	}
+
+
+	public String getColor() {
+		return color;
+	}
+
+
+	public void setColor(String color) {
+		this.color = color;
+	}
+	
+	public double getArea() {
+		return radius*radius*3.14;
+	}
+
+
+	@Override
+	public String toString() {
+		return "Circle [radius=" + radius + ", color=" + color + ", getRadius()=" + getRadius() + ", getColor()="
+				+ getColor() + "]";
+	}
+}
